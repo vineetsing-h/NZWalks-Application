935e56e3ea954e3930a171fc4e3e0cd110a49871
Vineet <vineetsingh4040@gmail.com>
2024-12-27 00:32:27 +0530

    Seeding Data using Entity Framework Core

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -15,5 +15,82 @@ namespace NZWalks.API.Data
         public DbSet<Region> Regions { get; set; }
 
         public DbSet<Walk> Walks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data for Difficulties
+            // Easy, Medium, Hard
+            var difficulties = new List<Difficulty>() 
+            { 
+                new Difficulty()
+                {
+                    Id = Guid.Parse("751882ce-03c1-41df-a7b3-f30065d26890"),
+                    Name = "Easy"
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("45f803b0-8bba-4fdc-ac4e-b0397d4966e2"),
+                    Name = "Medium"
+                },
+                new Difficulty()
+                {
+                    Id = Guid.Parse("a242a8d7-65eb-4de7-971f-4292f6e71eb0"),
+                    Name = "Hard"
+                }
+            };
+
+            // Seed difficulties to the database
+            modelBuilder.Entity<Difficulty>().HasData(difficulties);
+
+            // Seed data for Regions
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.Parse("a5d08149-31b3-49f2-88fe-f28f182f8526"),
+                    Name = "Auckland",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("5c207a38-18e0-4a28-b4fd-2e5eb9157b9a"),
+                    Name = "Northland",
+                    Code = "NTL",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("cc573506-64ac-43f8-88d6-1a28132154bc"),
+                    Name = "Bay Of Plenty",
+                    Code = "BOP",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("9ddac940-5d19-461b-8bb8-6595b33a4851"),
+                    Name = "Wellington",
+                    Code = "WGN",
+                    RegionImageUrl = "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("906cb139-415a-4bbb-a174-1a1faf9fb1f6"),
+                    Name = "Nelson",
+                    Code = "NSN",
+                    RegionImageUrl = "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("f077a22e-4248-4bf6-b564-c7cf4e250263"),
+                    Name = "Southland",
+                    Code = "STL",
+                    RegionImageUrl = null
+                },
+            };
+            modelBuilder.Entity<Region>().HasData(regions);
+        }
     }
 }
---
+++ NZWalks.API/Migrations/20241226185740_Seeding Data for Difficulties and Regions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingDataforDifficultiesandRegions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("45f803b0-8bba-4fdc-ac4e-b0397d4966e2"), "Medium" },
+                    { new Guid("751882ce-03c1-41df-a7b3-f30065d26890"), "Easy" },
+                    { new Guid("a242a8d7-65eb-4de7-971f-4292f6e71eb0"), "Hard" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("5c207a38-18e0-4a28-b4fd-2e5eb9157b9a"), "NTL", "Northland", null },
+                    { new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"), "NSN", "Nelson", "https://images.pexels.com/photos/13918194/pexels-photo-13918194.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("9ddac940-5d19-461b-8bb8-6595b33a4851"), "WGN", "Wellington", "https://images.pexels.com/photos/4350631/pexels-photo-4350631.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("a5d08149-31b3-49f2-88fe-f28f182f8526"), "AKL", "Auckland", "https://images.pexels.com/photos/5169056/pexels-photo-5169056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+                    { new Guid("cc573506-64ac-43f8-88d6-1a28132154bc"), "BOP", "Bay Of Plenty", null },
+                    { new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"), "STL", "Southland", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("45f803b0-8bba-4fdc-ac4e-b0397d4966e2"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("751882ce-03c1-41df-a7b3-f30065d26890"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("a242a8d7-65eb-4de7-971f-4292f6e71eb0"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("5c207a38-18e0-4a28-b4fd-2e5eb9157b9a"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("906cb139-415a-4bbb-a174-1a1faf9fb1f6"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("9ddac940-5d19-461b-8bb8-6595b33a4851"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("a5d08149-31b3-49f2-88fe-f28f182f8526"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("cc573506-64ac-43f8-88d6-1a28132154bc"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f077a22e-4248-4bf6-b564-c7cf4e250263"));
+        }
+    }
+}
