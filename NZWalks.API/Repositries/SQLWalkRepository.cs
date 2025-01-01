dfaeb6d469e32b5e84d23fb99d292ea8987bf372
Vineet <vineetsingh4040@gmail.com>
2025-01-02 00:13:41 +0530

    Sorting in ASP.NET Core Web API

--- NZWalks.API/Repositries/SQLWalkRepository.cs
+++ NZWalks.API/Repositries/SQLWalkRepository.cs
@@ -31,7 +31,8 @@ namespace NZWalks.API.Repositries
             return existingWalk;
         }
 
-        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null)
+        public async Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
+            string? sortBy=null, bool isAscending=true)
         {
             var walks = dbContext.Walks.Include("Difficulty").Include("Region").AsQueryable();
             // Filtering
@@ -40,6 +41,19 @@ namespace NZWalks.API.Repositries
                 if (filterOn.Equals("Name", StringComparison.OrdinalIgnoreCase))
                 {
                     walks = walks.Where(x => x.Name.Contains(filterQuery));
+                }
+            }
+
+            // Sorting
+            if(string.IsNullOrWhiteSpace(sortBy) == false)
+            {
+                if(sortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.Name): walks.OrderByDescending(x => x.Name);
+                }
+                else if(sortBy.Equals("Length", StringComparison.OrdinalIgnoreCase))
+                {
+                    walks = isAscending ? walks.OrderBy(x => x.LengthInKm) : walks.OrderByDescending(x => x.LengthInKm);
                 }
             }
 
