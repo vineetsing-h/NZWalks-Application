07bf3a4ce38b4f5814bc703c163bfdeb41eab196
Vineet <vineetsingh4040@gmail.com>
2025-01-01 23:31:26 +0530

    Filtering in ASP.NET Core Web API

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -43,11 +43,11 @@ namespace NZWalks.API.Controllers
 
 
         // GET Walk
-        // GET: /api/Walks
+        // GET: /api/Walks?filterOn=Name&filterQuery=Track
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn,[FromQuery] string? filterQuery)
         {
-            var walksDomainModel= await walkRepository.GetAllAsync();
+            var walksDomainModel= await walkRepository.GetAllAsync(filterOn, filterQuery);
             // Map Domain Model to DTO
             return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
         }
